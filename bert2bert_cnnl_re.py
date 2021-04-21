4b8038c7ef635aefe27cd4bb9352a553a9b42d29
Shane-Yuhe-Xin <78398390+Shane-Yuhe-Xin@users.noreply.github.com>
2021-04-21 11:20:33 -0400

    new bert2bert, use this

---
+++ bert2bert_cnnl_re.py
@@ -0,0 +1,181 @@
+# -*- coding: utf-8 -*-
+"""BERT2BERT encoder-decoder model, fine-tuning by cnn/dailymail dataset
+
+### **Data Preprocessing**
+"""
+
+
+
+
+# import datasets
+# import transformers
+
+from transformers import BertTokenizerFast
+
+tokenizer = BertTokenizerFast.from_pretrained("bert-base-uncased")
+tokenizer.bos_token = tokenizer.cls_token
+tokenizer.eos_token = tokenizer.sep_token
+
+train_data = datasets.load_dataset("cnn_dailymail", "3.0.0", split="train")
+val_data = datasets.load_dataset("cnn_dailymail", "3.0.0", split="validation[:10%]")
+
+batch_size= 16
+encoder_max_length=512
+decoder_max_length=128
+
+def process_data_to_model_inputs(batch):
+  # tokenize the inputs and labels
+  inputs = tokenizer(batch["article"], padding="max_length", truncation=True, max_length=encoder_max_length)
+  outputs = tokenizer(batch["highlights"], padding="max_length", truncation=True, max_length=decoder_max_length)
+
+  batch["input_ids"] = inputs.input_ids
+  batch["attention_mask"] = inputs.attention_mask
+  batch["decoder_input_ids"] = outputs.input_ids
+  batch["decoder_attention_mask"] = outputs.attention_mask
+  batch["labels"] = outputs.input_ids.copy()
+
+
+  batch["labels"] = [[-100 if token == tokenizer.pad_token_id else token for token in labels] for labels in batch["labels"]]
+
+  return batch
+
+
+train_data = train_data.map(
+    process_data_to_model_inputs, 
+    batched=True, 
+    batch_size=batch_size, 
+    remove_columns=["article", "highlights", "id"]
+)
+train_data.set_format(
+    type="torch", columns=["input_ids", "attention_mask", "decoder_input_ids", "decoder_attention_mask", "labels"],
+)
+
+
+
+val_data = val_data.map(
+    process_data_to_model_inputs, 
+    batched=True, 
+    batch_size=batch_size, 
+    remove_columns=["article", "highlights", "id"]
+)
+val_data.set_format(
+    type="torch", columns=["input_ids", "attention_mask", "decoder_input_ids", "decoder_attention_mask", "labels"],
+)
+
+"""### **Warm-starting the Encoder-Decoder Model**"""
+
+from transformers import EncoderDecoderModel
+
+bert2bert = EncoderDecoderModel.from_encoder_decoder_pretrained("bert-base-uncased", "bert-base-uncased")
+
+# set special tokens
+bert2bert.config.decoder_start_token_id = tokenizer.bos_token_id
+bert2bert.config.eos_token_id = tokenizer.eos_token_id
+bert2bert.config.pad_token_id = tokenizer.pad_token_id
+
+# sensible parameters for beam search
+bert2bert.config.vocab_size = bert2bert.config.decoder.vocab_size
+bert2bert.config.max_length = 142
+bert2bert.config.min_length = 56
+bert2bert.config.no_repeat_ngram_size = 3
+bert2bert.config.early_stopping = True
+bert2bert.config.length_penalty = 2.0
+bert2bert.config.num_beams = 4
+
+### **Fine-Tuning Warm-Started Encoder-Decoder Models**
+
+
+from transformers import Seq2SeqTrainingArguments, Seq2SeqTrainer
+
+
+# load rouge for validation
+rouge = datasets.load_metric("rouge")
+
+def compute_metrics(pred):
+    labels_ids = pred.label_ids
+    pred_ids = pred.predictions
+
+    # all unnecessary tokens are removed
+    pred_str = tokenizer.batch_decode(pred_ids, skip_special_tokens=True)
+    labels_ids[labels_ids == -100] = tokenizer.pad_token_id
+    label_str = tokenizer.batch_decode(labels_ids, skip_special_tokens=True)
+
+    rouge_output = rouge.compute(predictions=pred_str, references=label_str, rouge_types=["rouge2"])["rouge2"].mid
+
+    return {
+        "rouge2_precision": round(rouge_output.precision, 4),
+        "rouge2_recall": round(rouge_output.recall, 4),
+        "rouge2_fmeasure": round(rouge_output.fmeasure, 4),
+    }
+
+"""training"""
+
+# set training arguments - these params are not really tuned, feel free to change
+training_args = Seq2SeqTrainingArguments(
+    output_dir="./",
+    evaluation_strategy="steps",
+    per_device_train_batch_size=batch_size,
+    per_device_eval_batch_size=batch_size,
+    predict_with_generate=True,
+    logging_steps=1000, 
+    save_steps=500, 
+    eval_steps=8000, 
+    warmup_steps=2000,  
+    overwrite_output_dir=True,
+    save_total_limit=3,
+    fp16=True, 
+)
+
+# instantiate trainer
+trainer = Seq2SeqTrainer(
+    model=bert2bert,
+    tokenizer=tokenizer,
+    args=training_args,
+    compute_metrics=compute_metrics,
+    train_dataset=train_data,
+    eval_dataset=val_data,
+)
+trainer.train()
+
+### Evaluation
+
+
+import datasets
+from transformers import BertTokenizer, EncoderDecoderModel
+
+tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+model = EncoderDecoderModel.from_pretrained("./checkpoint-500")
+model.to("cuda")
+
+test_data = datasets.load_dataset("cnn_dailymail", "3.0.0", split="test")
+
+
+
+batch_size = 64 
+
+# map data correctly
+def generate_summary(batch):
+
+    inputs = tokenizer(batch["article"], padding="max_length", truncation=True, max_length=512, return_tensors="pt")
+    input_ids = inputs.input_ids.to("cuda")
+    attention_mask = inputs.attention_mask.to("cuda")
+
+    outputs = model.generate(input_ids, attention_mask=attention_mask)
+
+    # all special tokens including will be removed
+    output_str = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+
+    batch["pred"] = output_str
+
+    return batch
+
+results = test_data.map(generate_summary, batched=True, batch_size=batch_size, remove_columns=["article"])
+
+pred_str = results["pred"]
+label_str = results["highlights"]
+
+rouge_output = rouge.compute(predictions=pred_str, references=label_str, rouge_types=["rouge2"])["rouge2"].mid
+
+print(rouge_output)
+
+
