5a4b0d90156343933ae49afcd3416fa9e130d424
Shane-Yuhe-Xin <78398390+Shane-Yuhe-Xin@users.noreply.github.com>
2021-05-18 10:39:45 -0400

    test each dataset on bert2bert fintuned on xsum
    see the suffix:
    bill- tested on billsum
    r -- tested on reddit
    x -- tested on xsum

---
+++ berttrainxr.py
@@ -0,0 +1,69 @@
+
+
+# load rouge for validation
+import rouge_score
+import datasets
+rouge = datasets.load_metric("rouge")
+
+def compute_metrics(pred):
+    labels_ids = pred.label_ids
+    pred_ids = pred.predictions
+
+    # all unnecessary tokens are removed
+    pred_str = tokenizer.batch_decode(pred_ids, skip_special_tokens=True)
+    labels_ids[labels_ids == -100] = tokenizer.pad_token_id
+    label_str = tokenizer.batch_decode(labels_ids, skip_special_tokens=True)
+
+    rouge_output = rouge.compute(predictions=pred_str, references=label_str, rouge_types=["rouge2"])["rouge2"].mid
+
+    return {
+        "rouge2_precision": round(rouge_output.precision, 4),
+        "rouge2_recall": round(rouge_output.recall, 4),
+        "rouge2_fmeasure": round(rouge_output.fmeasure, 4),
+    }
+
+
+from transformers import BertTokenizer, EncoderDecoderModel
+
+tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+model = EncoderDecoderModel.from_pretrained("./checkpoint-30500")
+model.to("cuda")
+
+test_data = datasets.load_dataset("reddit", "3.0.0", split="train", ignore_verifications=True)
+test_data = test_data.select(range(192417))
+# only use 16 training examples for notebook - DELETE LINE FOR FULL TRAINING
+
+
+batch_size = 64  # change to 64 for full evaluation
+
+def generate_summary(batch):
+    # Tokenizer will automatically set [BOS] <text> [EOS]
+    # cut off at BERT max length 512
+    inputs = tokenizer(batch["content"], padding="max_length", truncation=True, max_length=512, return_tensors="pt")
+    input_ids = inputs.input_ids.to("cuda")
+    attention_mask = inputs.attention_mask.to("cuda")
+
+    outputs = model.generate(input_ids, attention_mask=attention_mask)
+
+    # all special tokens including will be removed
+    output_str = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+
+    batch["pred"] = output_str
+
+    return batch
+
+results = test_data.map(generate_summary, batched=True, batch_size=batch_size)
+
+pred_str = results["pred"]
+label_str = results["summary"]
+
+rouge_output = rouge.compute(predictions=pred_str, references=label_str, rouge_types=["rouge2"])["rouge2"].mid
+
+print(rouge_output)
+
+"""The fully trained *BERT2BERT* model is uploaded to the 🤗model hub under [patrickvonplaten/bert2bert_cnn_daily_mail](https://huggingface.co/patrickvonplaten/bert2bert_cnn_daily_mail). 
+
+The model achieves a ROUGE-2 score of **18.22**, which is even a little better than reported in the paper.
+
+For some summarization examples, the reader is advised to use the online inference API of the model, [here](https://huggingface.co/patrickvonplaten/bert2bert_cnn_daily_mail).
+"""
